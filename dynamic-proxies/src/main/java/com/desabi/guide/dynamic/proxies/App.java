ac031efec84016b63b508c7a289711282a13f139
desabi <desabi@live.com>
2025-08-25 08:20:33 -0600

    feat: dynamic proxies, base project

---
+++ dynamic-proxies/src/main/java/com/desabi/guide/dynamic/proxies/App.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.dynamic.proxies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
